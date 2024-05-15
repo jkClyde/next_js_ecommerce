89e9f1c488a06080c39eb4bfafdefc0f31081f4f
Clyde <jcayap18@gmail.com>
2024-05-16 00:59:07 +0800

    updated layout

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -61,7 +61,7 @@ export default async function CategoryPage({
                   </p>
                 </div>
                 <p className="text-sm font-medium text-gray-900">
-                  ${product.price}
+                  ₱{product.price}
                 </p>
               </div>
             </div>
