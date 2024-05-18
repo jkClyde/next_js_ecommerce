9e3fd8f700e8a38048378a9d48ae877e9b439df4
Clyde <jcayap18@gmail.com>
2024-05-19 01:21:03 +0800

    esh

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -55,7 +55,7 @@ export default async function Hero() {
       <div className="flex flex-col items-center justify-between gap-8 md:flex-row">
         <div className="flex h-12 w-64 divide-x overflow-hidden rounded-lg border">
           <Link
-            href="/Shirt"
+            href="/Shirts"
             className="flex w-1/2 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
           >
             Shirts
